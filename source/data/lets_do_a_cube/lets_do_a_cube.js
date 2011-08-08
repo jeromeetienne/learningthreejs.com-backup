7611cc53fab862d67b5cde3d4887349549f8c6d5
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-08 12:36:56 +0200

    source/data/lets_do_a_cube cleanup + Detector + Stats

--- source/data/lets_do_a_cube/lets_do_a_cube.js
+++ source/data/lets_do_a_cube/lets_do_a_cube.js
@@ -1,12 +1,14 @@
 // This source is the javascript needed to build a simple moving
-// cube in **three.js**. It is the source about this [blog post](/blog/2011/08/06/lets-do-a-cube/)
+// cube in **three.js** based on this
+// [example](https://raw.github.com/mrdoob/three.js/master/examples/canvas_geometry_cube.html)
+// It is the source about this [blog post](/blog/2011/08/06/lets-do-a-cube/).
 
 // ## Now lets start
 
 // declare a bunch of variable we will need later
 var startTime	= Date.now();
 var container;
-var camera, scene, renderer;
+var camera, scene, renderer, stats;
 var cube;
 
 // ## bootstrap functions
@@ -17,6 +19,9 @@ animate();
 
 // ## Initialize everything
 function init() {
+	// test if webgl is supported
+	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
 	// create the camera
 	camera = new THREE.Camera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.y = 150;
@@ -41,6 +46,12 @@ function init() {
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
+	
+	// init the Stats and append it to the Dom - performance vuemeter
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	container.appendChild( stats.domElement );
 }
 
 // ## Animate and Display the Scene
@@ -49,6 +60,8 @@ function animate() {
 	render();
 	// relaunch the 'timer' 
 	requestAnimationFrame( animate );
+	// update the stats
+	stats.update();
 }
 
 
