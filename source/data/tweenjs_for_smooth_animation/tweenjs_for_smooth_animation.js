4991621f2c77d6d3224d15e0006285069ce5da22
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-17 13:19:56 +0200

    source/data/tweenjs_for_smooth_animation more work

--- source/data/tweenjs_for_smooth_animation/tweenjs_for_smooth_animation.js
+++ source/data/tweenjs_for_smooth_animation/tweenjs_for_smooth_animation.js
@@ -11,6 +11,24 @@ var container;
 var camera, scene, renderer, stats;
 var cube;
 
+// maybe replace that by window... or something
+var userOpts	= {
+	range		: 450,
+	duration	: 2000,
+	delay		: 0,
+	easing		: TWEEN.Easing.Elastic.EaseInOut
+};
+
+// collect all available easing in TWEEN library
+var easings	= [];
+Object.keys(TWEEN.Easing).forEach(function(family){
+	Object.keys(TWEEN.Easing[family]).forEach(function(direction){
+		easings.push(family+'.'+direction);
+	});
+});
+console.log("easings", easings)
+
+
 // ## bootstrap functions
 // initialiaze everything
 init();
@@ -23,17 +41,39 @@ function init() {
 	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 	// create the camera
-	camera = new THREE.Camera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.y = 150;
-	camera.position.z = 350;
-	camera.target.position.y = 150;
+	camera = new THREE.Camera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 1000;
 
 	// create the Scene
 	scene = new THREE.Scene();
 
 	// create the Cube
 	cube = new THREE.Mesh( new THREE.CubeGeometry( 200, 200, 200 ), new THREE.MeshNormalMaterial() );
-	cube.position.y = 150;
+
+(function(){
+	var mesh	= cube;
+	var update	= function(){
+		mesh.position.x = position.x;
+	}
+
+	var position	= {x: 450};
+	var tween = new TWEEN.Tween(position)
+		.to({x: -450}, 3000)
+		.delay(1000)
+		.easing(TWEEN.Easing.Elastic.EaseInOut)
+		.onUpdate(update);
+
+	var tweenBack = new TWEEN.Tween(position)
+		.to({x: 450}, 2000)
+		.delay(1000)
+		.easing(TWEEN.Easing.Elastic.EaseInOut)
+		.onUpdate(update);
+
+	tween.chain(tweenBack);
+	tweenBack.chain(tween);
+	
+	tween.start();
+})();
 
 	// add the object to the scene
 	scene.addObject( cube );
@@ -62,20 +102,25 @@ function animate() {
 	requestAnimationFrame( animate );
 	// update the stats
 	stats.update();
+	// update the tweens from TWEEN library
+	TWEEN.update();
 }
 
 
 // ## Render the 3D Scene
 function render() {
+	var dtime	= Date.now()/1000;
 	// animate the cube
-	cube.rotation.x += 0.02;
-	cube.rotation.y += 0.0225;
-	cube.rotation.z += 0.0175;
+	if( false ){
+		cube.rotation.x += 0.02;
+		cube.rotation.y += 0.0225;
+		cube.rotation.z += 0.0175;		
+	}
 	// make the cube bounce
-	var dtime	= Date.now() - startTime;
-	cube.scale.x	= 1.0 + 0.3*Math.sin(dtime/300);
-	cube.scale.y	= 1.0 + 0.3*Math.sin(dtime/300);
-	cube.scale.z	= 1.0 + 0.3*Math.sin(dtime/300);
+	if( false ){
+		var scale	= 1.0 + 0.3*Math.sin(dtime*Math.PI);
+		cube.scale.set(scale, scale, scale);		
+	}
 	// actually display the scene in the Dom element
 	renderer.render( scene, camera );
 }
