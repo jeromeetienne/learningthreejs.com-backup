a318146a2cf35aeceae93d9a21bf58793a3afe6f
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-22 09:03:31 +0200

    lets_do_a_plasma clean up, removed the wobble experiment, and text

--- source/data/lets_do_a_plasma/lets_do_a_plasma.js
+++ source/data/lets_do_a_plasma/lets_do_a_plasma.js
@@ -61,26 +61,11 @@ function init() {
 
 	// create the camera
 	camera	= new THREE.Camera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.z	= 80;
-	camera.position.z	= 120;
+	camera.position.z	= 45;
+	//camera.position.z	= 200;
 
 	// create the Scene
 	scene	= new THREE.Scene();
-
-
-	var dirLight	= new THREE.DirectionalLight( 0x20a080, 1.0 );
-	dirLight.position.set( 0, 0, 1 );
-	dirLight.position.normalize();
-	scene.addLight( dirLight );
-	
-	
-	var pointLight	= new THREE.PointLight( 0x0101f0, 3.5 );
-	pointLight.position.set( 50, 50, 50 );
-	scene.addLight( pointLight );
-
-	var pointLight	= new THREE.PointLight( 0xF00180, 5 );
-	pointLight.position.set( -50, 0, 10 );
-	scene.addLight( pointLight );
 
 	// build the GUI 
 	buildGui(userOpts, function(){
@@ -102,62 +87,17 @@ function init() {
 	//var material	= new THREE.MeshNormalMaterial();
 	//var material	= new THREE.MeshPhongMaterial( { color: 0xaaaaaa } );
 
-	var geometry	= new THREE.CubeGeometry( 100, 100, 100 );
+	//var geometry	= new THREE.CubeGeometry( 100*4/3, 100, 100 );
+	var geometry	= new THREE.PlaneGeometry( 100*4/3, 100 );
 	//var geometry	= new THREE.TorusGeometry( 50, 20, 45, 45 );
-	var geometry	= new THREE.SphereGeometry( 50, 25, 25 );
-
-
-	//var geometry	= new THREE.TextGeometry("node.js", {
-	//	size		: 50,
-	//	height		: 20,
-	//	weight		: 'bold',
-	//	bezelThickness	: 10,
-	//	bezelSize	: 10,
-	//	bezelEnabled	: true
-	//});
-
-	THREEx.GeometryWobble.init(geometry);
-	THREEx.GeometryWobble.cpuAxis(geometry, 'x', 0.02);
-	
-	// add wireframe
-	material	= [material, new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true } )]; 
+	//var geometry	= new THREE.SphereGeometry( 50, 25, 25 );
 	
 // TODO make a plan facing camera instead
 	// create the Mesh
 	planeMesh	= new THREE.Mesh( geometry, material );
-
-	// to center the object
-	//planeMesh.geometry.computeBoundingBox();
-	//planeMesh.position.x	= -0.5 * ( planeMesh.geometry.boundingBox.x[ 1 ] - planeMesh.geometry.boundingBox.x[ 0 ] );
-	//planeMesh.position.y	= -0.5 * ( planeMesh.geometry.boundingBox.y[ 1 ] - planeMesh.geometry.boundingBox.y[ 0 ] );
-	//planeMesh.position.z	= -0.5 * ( planeMesh.geometry.boundingBox.z[ 1 ] - planeMesh.geometry.boundingBox.z[ 0 ] );
 	
 	// add the object to the scene
 	scene.addObject( planeMesh );
-
-
-// failed attempts at a generic geometryCenter()
-(function(geometry){
-	return;
-	//geometry.computeBoundingBox();
-
-	var delta	= new THREE.Vector3()
-	delta.x		= -( geometry.boundingBox.x[ 1 ] - geometry.boundingBox.x[ 0 ] ) / 2;
-	delta.y		= -( geometry.boundingBox.y[ 1 ] - geometry.boundingBox.y[ 0 ] ) / 2;
-	delta.z		= -( geometry.boundingBox.z[ 1 ] - geometry.boundingBox.z[ 0 ] ) / 2;
-console.log("delta", delta)
-console.log("boundingBox", geometry.boundingBox)
-	for(var i = 0; i < geometry.vertices.length; i++) {
-		var vertex	= geometry.vertices[i];
-		vertex.position.addSelf( delta )
-	}
-//geometry.computeBoundingBox();
-console.log("boundingBox", geometry.boundingBox)
-//geometry.computeCentroids();
-//geometry.computeFaceNormals();
-//geometry.computeVertexNormals();
-	geometry.__dirtyVertices = true;
-})(planeMesh.geometry);
 
 	// create the container element
 	container	= document.createElement( 'div' );
@@ -174,7 +114,7 @@ console.log("boundingBox", geometry.boundingBox)
 	// init the Stats and append it to the Dom - performance vuemeter
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.top = '0px';
+	stats.domElement.style.top	= '0px';
 	container.appendChild( stats.domElement );
 }
 
@@ -193,37 +133,36 @@ function animate() {
 */
 function animatePlasma(uniforms)
 {
-	var time	= Date.now()/1000;
-	uniforms.time.value	= time * userOpts.speed;	
-	uniforms.rotation.value	= Math.sin(time/10)*Math.PI;
-	uniforms.scale.value	= 0.4 - 0.2*Math.sin(time);
+	var seconds		= (Date.now() - startTime) / 1000;
+
+	uniforms.time.value	= seconds * userOpts.speed;	
+	uniforms.rotation.value	= Math.sin(seconds/10)*Math.PI;
+	uniforms.scale.value	= 0.4 - 0.2*Math.sin(seconds);
+	uniforms.opacity.value	= 1.0;
 
 	uniforms.c0.value	= userOpts.c0;	
 	uniforms.c1.value	= userOpts.c1;	
 	uniforms.c2.value	= userOpts.c2;	
 	uniforms.c3.value	= userOpts.c3;	
 	uniforms.c4.value	= userOpts.c4;	
-	uniforms.c5.value	= userOpts.c5;	
+	uniforms.c5.value	= userOpts.c5;
+	
+	console.log("uniforms", uniforms.time.value)
 }
 
 // ## Render the 3D Scene
 function render(){
-	// to animate the plasme
+	// to animate the plasma
 	animatePlasma(planeMesh.materials[0].uniforms);
-	
-	var time	= Date.now()/1000;
-
-	// to animate the geometry
-	THREEx.GeometryWobble.Animate(planeMesh.geometry, time/Math.PI*15, new THREE.Vector3(15,25, 00));
-	
+		
 	// animate the planeMesh
-	if( true ){
+	if( false ){
 		planeMesh.rotation.x += 0.005/2.5;
 		planeMesh.rotation.y += 0.0125/2.5;
 		planeMesh.rotation.z += 0.0085/2.5;
 	}
 	// make the planeMesh bounce
-	if( true ){
+	if( false ){
 		var dtime	= Date.now() - startTime;
 		planeMesh.scale.x	= 1.0 + 0.3*Math.sin(dtime/300);
 		planeMesh.scale.y	= 1.0 + 0.3*Math.sin(dtime/300);
