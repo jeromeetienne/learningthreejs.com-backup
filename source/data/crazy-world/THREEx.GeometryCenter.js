4e95d5607e810c08662901f5ab24d86f2788196c
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-24 09:42:41 +0200

    THREEx.GeometryCenter.js added scale+translate

--- source/data/crazy-world/THREEx.GeometryCenter.js
+++ source/data/crazy-world/THREEx.GeometryCenter.js
@@ -2,6 +2,13 @@ var THREEx		= THREEx || {};
 
 THREEx.GeometryCenter	= {};
 
+// TODO
+// - add translate
+// - add rotate
+// - add scale
+// - chained API
+// - possibility a matrix to reduce computation ?
+
 THREEx.GeometryCenter.center	= function(geometry, noX, noY, noZ)
 {
 	// make sure that geometry.boundingBox is uptodate
@@ -13,6 +20,26 @@ THREEx.GeometryCenter.center	= function(geometry, noX, noY, noZ)
 	if( !noY )	delta.y	= -( geometry.boundingBox.y[ 1 ] + geometry.boundingBox.y[ 0 ] ) / 2;
 	if( !noZ )	delta.z	= -( geometry.boundingBox.z[ 1 ] + geometry.boundingBox.z[ 0 ] ) / 2;
 
+	return this.translate(geometry, delta)
+}
+
+THREEx.GeometryCenter.scale	= function(geometry, scale)
+{
+	// change all geometry.vertices
+	for(var i = 0; i < geometry.vertices.length; i++) {
+		var vertex	= geometry.vertices[i];
+		vertex.position.multiplySelf(scale); 
+	}
+
+	// mark the vertices as dirty
+	geometry.__dirtyVertices = true;
+
+	// return this, to get chained API	
+	return this;
+}
+
+THREEx.GeometryCenter.translate	= function(geometry, delta)
+{
 	// change all geometry.vertices
 	for(var i = 0; i < geometry.vertices.length; i++) {
 		var vertex	= geometry.vertices[i];
@@ -21,4 +48,7 @@ THREEx.GeometryCenter.center	= function(geometry, noX, noY, noZ)
 
 	// mark the vertices as dirty
 	geometry.__dirtyVertices = true;
+
+	// return this, to get chained API	
+	return this;
 }
