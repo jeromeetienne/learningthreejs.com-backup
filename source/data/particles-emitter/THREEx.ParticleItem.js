3fe029d95224a52ee6c3993b925cef0ca6df13de
Jerome Etienne <jerome.etienne@gmail.com>
2011-08-28 14:19:43 +0200

    more work

--- source/data/particles-emitter/THREEx.ParticleItem.js
+++ source/data/particles-emitter/THREEx.ParticleItem.js
@@ -15,23 +15,33 @@ THREEx.Particle.item.prototype._reset	= function()
 	this._position	= new THREE.Vector3( 0,0,0 );
 	var position	= this._position;
 
-	position.x	= Math.random()*2-1;
+	//position.x	= Math.random()*2-1;
 	//position.y	= Math.random()*2-1;
-	position.z	= Math.random()*2-1;
+	//position.z	= Math.random()*2-1;
 	position.normalize();
 	//position.multiplyScalar( randomRange(0,0) );
+	
+(function(){
+	var angle	= randomRange( (90-15) * Math.PI/180, (90+15) * Math.PI/180);
+	position.x	= Math.cos(angle);
+	position.y	= Math.sin(angle);
+	position.z	= 0;
+	position.normalize().multiplyScalar( randomRange(1,1) );
+})();
 	
 	this._deletedAt		= Date.now() + 2000;
 
 	this._acceleration	= new THREE.Vector3(0, 0, 0);
+	this._friction		= new THREE.Vector3(1.0, 1.0, 1.0);
 
 	this._speed		= new THREE.Vector3();
 	this._speed.copy(this._position).normalize();
-	this._speed.y		= randomRange(1,3)
+	//this._speed.y		= randomRange(3,3)
 	//this._speed.x		= randomRange(-1, 1);
 	//this._speed.y		= randomRange(0.4, .5);
 	//this._speed.x		= randomRange(-1, 1);
-	this._speed.normalize().multiplyScalar( randomRange(1, 5) );
+	this._speed.normalize().multiplyScalar( randomRange(1, 3) );
+	//this._speed.normalize().multiplyScalar( 3 );
 
 	this._gravity		= new THREE.Vector3(0, -0.05, 0);	
 }
@@ -43,6 +53,7 @@ THREEx.Particle.item.prototype.update	= function()
 	
 	this._speed	.addSelf( this._acceleration );
 	this._speed	.addSelf( this._gravity );
+	this._speed	.multiplySelf( this._friction );
 	this._position	.addSelf( this._speed );
 }
 
