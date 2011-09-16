0c6a65c3292bd6c64aa856895cf5a9974ef39cdd
Jerome Etienne <jerome.etienne@gmail.com>
2011-09-16 04:42:57 +0200

    source/data/THREEx/THREEx.screenshot.js more comment

--- source/data/THREEx/THREEx.screenshot.js
+++ source/data/THREEx/THREEx.screenshot.js
@@ -88,6 +88,7 @@ var THREEx	= THREEx 		|| {};
 	var bindKey	= function(renderer, opts){
 		// handle parameters
 		opts		= opts		|| {};
+// FIXME this modification of opts parameters is a bug. remove it
 		opts.charCode	= opts.charCode	|| 'p'.charCodeAt(0);
 		opts.width	= opts.width	|| 640;
 		opts.height	= opts.height	|| 480;
@@ -101,6 +102,8 @@ var THREEx	= THREEx 		|| {};
 			if( event.which !== opts.charCode )	return;
 			// get the renderer output
 			var dataUrl	= this.toDataURL(renderer);
+// FIXME dont resize if not explicitly asked
+// * resize == async so if callback is a window open, it triggers the pop blocker
 			// resize it and notify the callback
 			_aspectResize(dataUrl, opts.width, opts.height, opts.callback);
 		}.bind(this);
